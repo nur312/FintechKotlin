ee3ffb64e391f991edfb58c33a73ac97546f1b57
nur312 <nurzhigit.en@gmail.com>
2022-03-10 20:53:30 +0300

    hw3 was corrected

--- src/test/kotlin/ru/tinkoff/fintech/homework1/ensemble/EnsembleTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework1/ensemble/EnsembleTest.kt
@@ -16,38 +16,30 @@ internal class EnsembleTest {
     fun `проверяем исполнение`() {
 
         /*2) С помощью тест-дублеров(Mock или Spy) настроить заглушку для тестирования.
-
         Выбрал Mock, так как в Ensemble вызываются только два метода из класса Dog.
-
         Заглушка должна быть настроена как минимум для 2-х наборов входных данных.
-
         Настоил для методов getSpeech() и makeSound() класаа Dog*/
 
         val dog = mockk<Dog>()
-
         every { dog.getSpeech() } returns "Dog Джо:\nАнсамбль\nНачинай\n"
-
         every { dog.makeSound() } returns "Dog Джо: \n\tгыгы\n"
-
+        val ensemble = Ensemble(Cat("Кетрин"), dog)
+        val expected = "Dog Джо:\n" +
+                "Ансамбль\n" +
+                "Начинай\n" +
+                "Ensemble's musicCat Кетрин:\n" +
+                "\tmur\n" +
+                "\tmur\n" +
+                "Dog Джо: \n" +
+                "\tгыгы\n" +
+                "Cat Кетрин:\n" +
+                "\tmeow\n"
         /*3) Настроенную заглушку передавать в конструктор класса из задания 4 лекции 1. Написать unit-тесты,
         проверяющие функционал класса, принимающего заглушку в качестве параметра в конструкторе.*/
 
-        val ensemble = Ensemble(Cat("Кетрин"), dog)
+        val music = ensemble.makeMusic()
 
-        assertEquals(
-            "Dog Джо:\n" +
-                    "Ансамбль\n" +
-                    "Начинай\n" +
-                    "Ensemble's musicCat Кетрин:\n" +
-                    "\tmur\n" +
-                    "\tmur\n" +
-                    "Dog Джо: \n" +
-                    "\tгыгы\n" +
-                    "Cat Кетрин:\n" +
-                    "\tmeow\n",
-            ensemble.makeMusic()
-        )
-
+        assertEquals(expected, music)
         /*4) С помощью ф-ии verify протестировать кол-во вызовов заглушки.*/
         verify(exactly = 1) { dog.getSpeech() }
         verify(exactly = 1) { dog.makeSound() }
@@ -55,8 +47,12 @@ internal class EnsembleTest {
 
     @Test
     fun `проверяем правильно ли составляются списки участников`() {
-        val ensemble = Ensemble(Cat("Кетрин"), Dog("Дореан"))
 
-        assertEquals(ensemble.getListOfParticipants(), listOf("Кетрин", "Дореан"))
+        val ensemble = Ensemble(Cat("Кетрин"), Dog("Дореан"))
+        val expected = listOf("Кетрин", "Дореан")
+
+        val listOfParticipants = ensemble.getListOfParticipants()
+
+        assertEquals(expected, listOfParticipants)
     }
 }
--- src/test/kotlin/ru/tinkoff/fintech/homework1/pets/DogTest.kt
+++ src/test/kotlin/ru/tinkoff/fintech/homework1/pets/DogTest.kt
@@ -14,36 +14,39 @@ internal class DogTest {
 
     @Test
     fun `проверяем случай, когда собака не знает ни однгог слова`() {
-        val dog = Dog("Кеша")
 
+        val dog = Dog("Кеша")
         val expected = ""
 
-        assertEquals(expected, dog.getSpeech())
+        val speech = dog.getSpeech()
+
+        assertEquals(expected, speech)
     }
 
     @Test
     fun `проверяем запоминаются ли слова собакой`() {
-        val dog = Dog("Кеша")
 
+        val dog = Dog("Кеша")
         dog.memorizeWord("доллар")
         dog.memorizeWord("остановись")
-
         val expected = "Dog Кеша:\nдоллар\nостановись\n"
 
-        assertEquals(expected, dog.getSpeech())
+        val speech = dog.getSpeech()
+
+        assertEquals(expected, speech)
     }
 
     @Test
     fun `проверяем не запоминает ли собака слишком много слов`() {
 
         val dog = Dog("Кеша", maxMemorizeWordCount = 2)
-
         dog.memorizeWord("доллар")
         dog.memorizeWord("остановись")
         dog.memorizeWord("уже")
-
         val expected = "Dog Кеша:\nдоллар\nостановись\n"
 
-        assertEquals(expected, dog.getSpeech())
+        val speech = dog.getSpeech()
+
+        assertEquals(expected, speech)
     }
 }
